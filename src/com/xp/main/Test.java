d983021be51a4d806fe20b30ae0e3033701be387
admin <succ_secret@163.com>
2021-04-17 20:14:48 +0800

    javaDemo 第一次测试

---
+++ src/com/xp/main/Test.java
@@ -0,0 +1,13 @@
+package com.xp.main;
+
+/**
+ * @version 1.0
+ * @Auther :xyp *
+ * @Description
+ * @CrateTime :2021/4/16  17:03
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
